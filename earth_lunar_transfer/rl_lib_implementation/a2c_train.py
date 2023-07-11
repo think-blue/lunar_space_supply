e4cd527a1c00333cbcda27436b2f3c1d9462d273
chinmaydhawan <maildrop.chinmay@hmail.com>
2023-07-11 14:04:39 +0530

    moved from old to new env state

--- earth_lunar_transfer/rl_lib_implementation/a2c_train.py
+++ earth_lunar_transfer/rl_lib_implementation/a2c_train.py
@@ -1,14 +1,22 @@
 import ray
-from ray.rllib.algorithms import a2c
+from ray.rllib.algorithms.a2c import A2CConfig
 import json
 from gym_compatible_env import LunarEnvironment
 
 with open("env_config.json", "rb") as config_file:
     env_config = json.load(config_file)
 
-ray.init()
-algo = a2c.A2C(env=LunarEnvironment, config={
-    "env_config": env_config})
+algo = (
+    A2CConfig()
+    .rollouts(num_rollout_workers=10)
+    .resources(num_gpus=0)
+    .environment(env=LunarEnvironment, env_config=env_config)
+    .build()
 
-while True:
+)
+
+# algo = a2c.A2C(env=LunarEnvironment, config={
+#     "env_config": env_config})
+
+for _ in range(1000):
     print(algo.train())
