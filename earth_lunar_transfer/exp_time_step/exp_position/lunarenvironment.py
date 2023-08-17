1c82d7408a7ae8fe912c36c500551be454e667ad
chinmaydhawan <maildrop.chinmay@hmail.com>
2023-08-17 14:57:05 +0530

    added terminal conditions

--- earth_lunar_transfer/exp_time_step/exp_position/lunarenvironment.py
+++ earth_lunar_transfer/exp_time_step/exp_position/lunarenvironment.py
@@ -11,6 +11,7 @@ from earth_lunar_transfer.reference_exp.lunarenvironment import LunarEnvironment
 
 class LunarEnvPosition(LunarEnvironment):
     def __init__(self, env_config):
+
         LunarEnvironment.__init__(self, env_config=env_config)
 
 
@@ -20,6 +21,34 @@ class LunarEnvPosition(LunarEnvironment):
         exceeding time step is penalised
         discount factor is there
         """
+
+        position_threshold = 10000
+        velocity_threshold = 40
+
+        goal_achieved_reward = 0
+        if np.linalg.norm(self.delta_position) <= position_threshold:
+            self.terminated_condition = True
+            goal_achieved_reward = 20
+
+        time_penalty = 0
+        if self.time_step > self.max_time_steps:
+            self.truncated_condition = True
+            time_penalty = -10
+
+        fuel_penalty = 0
+        if self.fuel_mass < 0:
+            self.truncated_condition = True
+            fuel_penalty = -10
+
+        regions_penalty = 0
+        if np.linalg.norm(self.spacecraft_position - self.source_planet.eph(self.current_epoch)[0]) < 300e3:
+            regions_penalty = -10
+            self.truncated_condition = True
+
+        if np.linalg.norm(self.spacecraft_position - self.destination_planet.eph(self.current_epoch)[0]) < 1000e3:
+            regions_penalty = -10
+            self.truncated_condition = True
+
         # static destination based on the end epoch
         dest_position = self.target_position
         position_error = (self.spacecraft_position - dest_position)
@@ -27,12 +56,10 @@ class LunarEnvPosition(LunarEnvironment):
                 self.EARTH_MOON_MEAN_DISTANCE - self.destination_object_orbit_radius)
         positional_reward = - positional_error_magnitude
 
-        time_penalty = 0
-        if self.time_step > self.max_time_steps:
-            time_penalty = -10
 
-        discounted_reward = 1 * math.pow(self.env_config["discount_factor"], self.time_step)
-        reward = discounted_reward + positional_reward + time_penalty
+        discounted_reward = 2 * math.pow(self.env_config["discount_factor"], self.time_step)
+        reward = discounted_reward + positional_reward + time_penalty + goal_achieved_reward + fuel_penalty + regions_penalty
         # print(positional_reward, mass_reward, velocity_reward)
-        reward_components = [discounted_reward, positional_reward, time_penalty]
-        return reward, reward_components
+        reward_components = [discounted_reward, positional_reward, time_penalty, goal_achieved_reward, fuel_penalty, regions_penalty]
+
+        return reward, reward_components, self.truncated_condition, self.terminated_condition
