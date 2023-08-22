db9ec0307a15b5be9124b41d4c86c99a6d8ddd1f
Divye Singh <in-divye.singh@Divyes-MacBook-Pro.local>
2023-08-22 19:52:04 +0530

    [Divye] updated ddpg for lunar env and added mlflow and tensorboard

--- earth_lunar_transfer/agents/ddpg_agent.py
+++ earth_lunar_transfer/agents/ddpg_agent.py
@@ -10,17 +10,16 @@ from torch.optim import Adam
 from earth_lunar_transfer.network.actor import ActorNN
 from earth_lunar_transfer.network.critic import CriticNN
 from .constants import AgentConstants
-from earth_lunar_transfer.config import Config
 
-from .metrices import Metric
 from .memory import Memory
-from .utils import soft_update, hard_update, to_tensor, to_numpy, batchify, \
+from .utils import soft_update, hard_update, to_tensor, to_numpy, \
     LONG, INT32, USE_CUDA, use_device
 from .noise import OrnsteinUhlenbeckActionNoise
 import logging
+import mlflow
+from tensorboardX import SummaryWriter
 
 criterion = nn.MSELoss()
-
 
 class DDPGAgentGNN:
     ACTOR_LEARNING_RATE = AgentConstants.ACTOR_LEARNING_RATE
@@ -31,29 +30,29 @@ class DDPGAgentGNN:
 
     BATCH_SIZE = AgentConstants.BATCH_SIZE
     BUFFER_SIZE = AgentConstants.BUFFER_SIZE
-    EXPLORATION_EPISODES = AgentConstants.EXPLORATION_EPISODES
+    # EXPLORATION_EPISODES = AgentConstants.EXPLORATION_EPISODES
 
     def __init__(
-            self, env, weights_path, complex_path=None,
-            warmup=32, prate=0.00005, is_training=1
+            self, env, agent_config, tb_log_dir, weights_path,
+            complex_path=None, warmup=32, prate=0.00005, is_training=1
     ):
-        config = Config.get_instance()
-        self.metrices = Metric.get_instance()
+        self.config = agent_config
         self.input_shape = env.input_shape
         self.action_shape = env.action_space.n_outputs
         self.eps = 1.0
         self.decay_epsilon = 1/50000
         self.action_bounds = env.action_space.action_bounds
         self.memory = Memory(self.BUFFER_SIZE)
+        self.tb_writer = SummaryWriter(tb_log_dir)
         self.env = env
 
         self.noise = OrnsteinUhlenbeckActionNoise(
             mu=np.zeros(self.action_shape),
             dimension=self.action_shape,
-            num_steps=config.n_episodes
+            num_steps=self.config["n_episodes"]
         )
 
-        self.warm_up_steps = warmup
+        self.warm_up_steps = max(warmup, self.BATCH_SIZE)
         self.is_training = is_training
 
         self._actor = ActorNN(
@@ -129,15 +128,16 @@ class DDPGAgentGNN:
         training_loss = None
         
         for batch in batches:
-            complex_batched = batchify(batch.states)
-            next_complex_batched = batchify(batch.next_states)
+            complex_batched = batch.states[0]
+            next_complex_batched = batch.next_states[0]
 
-            actions = to_tensor(batch.actions, dtype=LONG)
+            actions = to_tensor(batch.actions)
             rewards = to_tensor(batch.rewards)
             terminals = to_tensor(batch.terminals, dtype=INT32)
+            actions = use_device(actions)
             complex_batched = use_device(complex_batched)
             next_complex_batched = use_device(next_complex_batched)
-
+            # import pdb; pdb.set_trace()
             loss = loss_function(
                 complex_batched,
                 actions,
@@ -147,6 +147,7 @@ class DDPGAgentGNN:
             )
             
             training_loss = loss if training_loss is None else training_loss + loss
+            actions.cpu()
             complex_batched.cpu()
             next_complex_batched.cpu()
 
@@ -172,7 +173,7 @@ class DDPGAgentGNN:
         soft_update(self._actor_target, self._actor, self.TAU)
         soft_update(self._critiq_target, self._critiq, self.TAU)
 
-        print("Value Loss: ", mean_q_loss, " policy loss: ", mean_policy_loss)
+        print("Critic Loss: ", mean_q_loss, " Actor loss: ", mean_policy_loss)
         return to_numpy(mean_q_loss), to_numpy(mean_policy_loss)
 
     def get_predicted_action(self, data, step=None, decay_epsilon=True):
@@ -194,103 +195,101 @@ class DDPGAgentGNN:
         action *= self.action_bounds[1]  # Remove as the multiplier has been removed
         return np.clip(action, *self.action_bounds)
 
-    def play(self, num_train_episodes):
-        config = Config.get_instance()
-        returns = []
-        num_steps = 0
-        max_episode_length = config.episode_length
-        max_reward = 0
-        i_episode = 0
+    def play(self, num_train_episodes, mlflow_run_id=None):
+        with mlflow.start_run(run_id=mlflow_run_id, nested=True) as child_run:
+            returns = []
+            num_steps = 0
+            max_reward = 0
+            i_episode = 0
 
-        while i_episode < num_train_episodes:
-            state, _ = self.env.reset()
-            critic_losses = []
-            actor_losses = []
+            while i_episode < num_train_episodes:
+                state, _ = self.env.reset()
+                critic_losses = []
+                actor_losses = []
 
-            molecule_index = 0
-            self.metrices.init_score(
-                i_episode, f'run_{i_episode}',
-                0,
-                [0, 0, 0]
-            )
+                episode_return, episode_length, d_store = [], 0, False
 
-            episode_return, episode_length, d_store = 0, 0, False
-
-            while not d_store:
-                
-                data = torch.tensor(state)
-                data = use_device(data)
-                predicted_action = self.get_predicted_action(
-                    data, episode_length
-                )
-                data.cpu()
-                action = self.get_action(predicted_action)
-
-                # reward, terminal = self.env.step(action)
-                next_state, reward, terminated, truncated, info = self.env.step(action)
-
-                # self.metrices.cache_score(i_episode, self.env.reward.score, molecule_index)
-                self.metrices.cache_action_reward(
-                    i_episode, action, reward, molecule_index
-                )
-                # self.metrices.cache_divergence(i_episode, terminal, molecule_index)
-
-                d_store = terminated or truncated
-                # reward = 0 if episode_length == max_episode_length else reward
-                self.memorize(data, [predicted_action], reward, torch.tensor(next_state), d_store)
-
-                episode_return += reward
-                episode_length += 1
-
-                self.log(action, np.round(reward, 4), episode_length, i_episode)
-
-                if num_steps > self.warm_up_steps:
-                    critic_loss, actor_loss = self.update_network()
-                    self.metrices.cache_loss(
-                        i_episode, actor_loss, critic_loss
+                while not d_store:
+                    data = torch.tensor(state.astype(np.float32).reshape(1,self.input_shape))
+                    data = use_device(data)
+                    predicted_action = self.get_predicted_action(
+                        data, episode_length
                     )
+                    data.cpu()
+                    action = self.get_action(predicted_action.copy())
+                    # import pdb; pdb.set_trace()
 
-                    critic_losses.append(critic_loss)
-                    actor_losses.append(actor_loss)
+                    # reward, terminal = self.env.step(action)
+                    next_state, reward, terminated, truncated, info = self.env.step(action)
+                    mlflow.log_metric("step_reward", reward, num_steps)
 
-                num_steps += 1
+                    d_store = terminated or truncated
+                    # reward = 0 if episode_length == max_episode_length else reward
+                    next_data = torch.tensor(next_state.astype(np.float32).reshape(1,self.input_shape))
+                    self.memorize(data, [action], reward, next_data, d_store)
 
-            returns.append([
-                i_episode + 1,
-                episode_length,
-                episode_return,
-                np.mean(critic_losses),
-                np.mean(actor_losses)
-            ])
+                    state = next_state.copy()
+                    episode_return.append(reward)
+                    episode_length += 1
 
-            max_reward = max_reward if max_reward > episode_return else episode_return
+                    self.log(action, np.round(reward, 4), episode_length, i_episode)
 
-            print(
-                f"Episode: {i_episode + 1} \
-                Return: {episode_return} \
-                episode_length: {episode_length} \
-                Max Reward; {max_reward} \
-                Critic Loss: {np.mean(critic_losses)} \
-                Actor loss: {np.mean(actor_losses)}"
-            )
+                    if num_steps > self.warm_up_steps:
+                        critic_loss, actor_loss = self.update_network()
+                        self.log_gradients_in_model(self._critiq, num_steps)
+                        self.log_gradients_in_model(self._critiq_target, num_steps)
+                        self.log_gradients_in_model(self._actor, num_steps)
+                        self.log_gradients_in_model(self._actor_target, num_steps)
+                        
+                        mlflow.log_metric("step_critic_loss", critic_loss, num_steps)
+                        mlflow.log_metric("step_actor_loss", actor_loss, num_steps)
 
-            logging.info(
-                f"Episode: {i_episode + 1} \
-                Return: {episode_return} \
-                episode_length: {episode_length} \
-                Max Reward; {max_reward} \
-                Critic Loss: {np.mean(critic_losses)} \
-                Actor loss: {np.mean(actor_losses)}"
-            )
+                        critic_losses.append(critic_loss)
+                        actor_losses.append(actor_loss)
 
-            if i_episode >= config.save_model_from and i_episode % 10 == 0:
-                self.save_weights(self.weights_path, i_episode)
-                self.metrices.plot_score_trend(i_episode, self.complex_path)
-                Metric.save(self.metrices, self.complex_path)
+                    num_steps += 1
 
-            i_episode += 1
+                mlflow.log_metric("episode_reward", sum(episode_return), i_episode)
+                mlflow.log_metric("episode_average_reward", np.mean(episode_return), i_episode)
+                mlflow.log_metric("episode_min_reward", min(episode_return), i_episode)
+                mlflow.log_metric("episode_max_reward", max(episode_return), i_episode)
+                mlflow.log_metric("episode_average_critic_loss", critic_loss, num_steps)
+                mlflow.log_metric("episode_average_actor_loss", actor_loss, num_steps)
+                
+                returns.append([
+                    i_episode + 1,
+                    episode_length,
+                    sum(episode_return),
+                    np.mean(critic_losses),
+                    np.mean(actor_losses)
+                ])
 
-        return returns
+                max_reward = max_reward if max_reward > sum(episode_return) else sum(episode_return)
+
+                print(
+                    f"Episode: {i_episode + 1} \
+                    Return: {sum(episode_return)} \
+                    episode_length: {episode_length} \
+                    Max Reward; {max_reward} \
+                    Critic Loss: {np.mean(critic_losses)} \
+                    Actor loss: {np.mean(actor_losses)}"
+                )
+
+                logging.info(
+                    f"Episode: {i_episode + 1} \
+                    Return: {sum(episode_return)} \
+                    episode_length: {episode_length} \
+                    Max Reward; {max_reward} \
+                    Critic Loss: {np.mean(critic_losses)} \
+                    Actor loss: {np.mean(actor_losses)}"
+                )
+
+                if i_episode >= self.config["save_model_from"] and i_episode % 10 == 0:
+                    self.save_weights(self.weights_path, i_episode)
+
+                i_episode += 1
+
+            return returns
 
     def log(self, action, reward, episode_length, i_episode):
         print(
@@ -307,6 +306,11 @@ class DDPGAgentGNN:
               {self.env.reward}"
         )
 
+    def log_gradients_in_model(self, model, step):
+        for tag, value in model.named_parameters():
+            if value.grad is not None:
+                self.tb_writer.add_histogram(tag + "/grad", value.grad.cpu(), step)
+    
     def save_weights(self, path, episode):
         torch.save(self._actor.state_dict(), f'{path}_{episode}_actor')
         torch.save(self._critiq.state_dict(), f'{path}_{episode}_critic')
