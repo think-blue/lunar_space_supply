4f2192abc2b34740e436879b39d74a11354b938e
chinmaydhawan <maildrop.chinmay@hmail.com>
2023-08-29 13:00:51 +0530

    added other training params

--- earth_lunar_transfer/a2c_resume_from_checkpoint.py
+++ earth_lunar_transfer/a2c_resume_from_checkpoint.py
@@ -1,29 +1,38 @@
 from ray.rllib.algorithms import Algorithm
-from lunarenvironment import LunarEnvironment
+from earth_lunar_transfer.exp_time_step.exp_position.lunarenvironment import LunarEnvPosition
+from earth_lunar_transfer.exp_time_step.exp_no_gravity.lunarenvironment_negative import LunarEnvNoGravity
 import json
 from datetime import datetime
 from ray.rllib.algorithms.a2c import A2CConfig
+from ray.rllib.algorithms.ppo import PPOConfig
 
+with open("env_config_train.json", "rb") as config_file:
+    env_config = json.load(config_file)
 
-checkpoint = "/home/chinmayd/ray_results/A2C_LunarEnvironment_2023-07-28_13-23-0146g1wieu/checkpoint_000401"
+checkpoint = "/home/chinmayd/ray_results/PPO_LunarEnvNoGravity_2023-08-23_19-33-27a_9w5khq/checkpoint_000301"
+# /home/chinmayd/ray_results/A2C_LunarEnvNoGravity_2023-08-21_19-30-12qja6p5h8/checkpoint_007201
 a2c_config = (
-    A2CConfig()
-    .environment(env=LunarEnvironment, env_config=env_config)
-    .rollouts(num_rollout_workers=2, num_envs_per_worker=2)
-    .resources(num_gpus=1)
-    .training(train_batch_size=500)
+    PPOConfig()
+    .environment(env=LunarEnvNoGravity, env_config=env_config)
+    .training(grad_clip=3, lr_schedule=[[0, 0.00002],  # 0.00002 this works
+                                        [2e6, 0.00000004],
+                                        [20000000, 0.000000000001]])
+    .rollouts(num_rollout_workers=5, num_envs_per_worker=5)
     .evaluation(evaluation_num_workers=1)
+    .framework("torch")
+    .debugging(log_level="INFO")
+    .offline_data(output="/nvme/lunar_space_supply/data/training_data/logs")
 )
 
 a2c_algo = a2c_config.build()
 a2c_algo.restore(checkpoint_path=checkpoint)
 
-for i in range(3201):
+for i in range(8005):
     train_results = a2c_algo.train()
     if i % 50 == 0:
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
         print(f"{current_time}: trained {i + 1} epochs")
-    if i % 400 == 0:
+    if i % 100 == 0:
         path = a2c_algo.save()
         print(f"saved to {path}")
